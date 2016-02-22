68f7f3d1e2a105497b3bd4002c1fe557fbe96c7e
shirk <shirk@192.168.222.1>
2016-02-22 09:27:29 +0200

    Test

--- ListDevicesApps/src/test/java/example/ExampleTest.java
+++ ListDevicesApps/src/test/java/example/ExampleTest.java
@@ -7,17 +7,21 @@ import org.testng.annotations.Test;
 import utility.BuildDevicesApps;
 
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.AfterTest;
 
 public class ExampleTest {
-   @Test
-  // @Parameters({"host","username","password"})	
+  @Test
+  /*public void testDevicesApps() {
+	  new BuildDevicesApps();
+  }*/
   public void testDevicesApps() {
-	   System.out.println(System.getProperty("host"));
-
-	  new BuildDevicesApps(System.getProperty("host"),System.getProperty("username"),System.getProperty("password"));
+	    String remoteHost = System.getProperty("BAR");
+		if (remoteHost == null) remoteHost = "http://localhost:4444/wd/hub";
+		
+		System.out.println(remoteHost);
+		
   }
+  
   @BeforeTest
   public void beforeTest() {
 	 System.out.println( "Collecting devices apps on cloud");
